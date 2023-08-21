b035c31d15e4cdd812c65dd2d8dcc68dd7d9289b
Dragos Tanta <dragos.tanta@gmail.com>
2023-08-21 11:37:51 +0300

    inital commit

---
+++ src/main/java/pages/MenuPage.java
@@ -0,0 +1,26 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class MenuPage {
+	
+	public WebDriver driver;
+	
+	public MenuPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	//locatori sau webelements
+	//WebElement buton =  driver.findElement(By.ceva(""))
+	public By homeLink = By.linkText("HOME");
+	public By contactLink = By.linkText("CONTACTS");
+	public By loginLink = By.linkText("Login");
+	
+	//metode specifice cu locatori sau elementele de mai sus
+	//metode specifice inseamna functionalitate a paginii in care ma aflu
+	public void navigateTo(By locator) {
+		driver.findElement(locator).click();
+	}
+	
+}
